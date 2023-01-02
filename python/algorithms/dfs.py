d6ff9e27163a7ee4966829012005cede3e29ec3b
Mary Montgomery <mary.montgomery31@gmail.com>
2023-01-02 12:44:12 -0500

    updated dfs py file

--- python/algorithms/dfs.py
+++ python/algorithms/dfs.py
@@ -1,15 +1,34 @@
-#create container for nodes 
-#[[node, [reachable nodes]], [node, [reachable nodes]]]
-#graph = []
+'''
+	description of file:
+	implement the linear time strongly connected components 
+	algorithm (depth first search algo).
+    Given a directed graph G you first create the
+	reverse graph Gr in linear time. Next, you run the 
+	undirected connected components method, processing the 
+	vertices in decreasing post order. You must make sure that 
+	both steps can be carried out in O(|V|+|E|) time.
+'''
+
 from collections import defaultdict
 
+#create container for nodes
+#[[node, [reachable nodes]], [node, [reachable nodes]]]
+#graph = []
 graph = defaultdict(list)
 
-
+'''
+addEdge
+function to add an edge to the graph list
+'''
 def addEdge(graph, u, v):
     graph[u].append(v)
  
-    # A function used by DFS
+'''
+DFSRecursive
+a helper function to DFS
+computes the recursion through all connected vertices of a given vertex
+to make sure all vertices are visited
+'''
 def DFSRecursive(arr, graph, v, visited):
  	# Mark the current node as visited and print it
     visited[v]= True
@@ -27,15 +46,19 @@ def DFSRecursive(arr, graph, v, visited):
  #   print("\ngraph: ", graph, "\n")
  #   print("\nvisited: ", visited, "\n")
     
- 
     for i in graph[v]:
     	if (i <= len(visited)):
 	        if visited[i] == False:
 	        	DFSRecursive(arr, graph, i, visited)
  		
- 
-    # The function to do DFS traversal. It uses
-    # recursive DFSRecursive()
+'''
+DFS
+the depth first search algorithm.
+we start from a root node and visit all possible nodes connected to this
+root node, going down the tree, until we need to backtrack. We improve
+efficiency by having a visited bool value so we don't traverse the same
+node structure multiple times.
+'''
 def DFS(arr, graph):
 	
     V = len(graph)+2  #total vertices
@@ -50,7 +73,11 @@ def DFS(arr, graph):
         if visited[i] == False:
             DFSRecursive(arr, graph, i, visited)
 
-
+'''
+scc
+creates an array of every child in the tree,
+removing nodes as it traverses.
+'''
 def scc(graph, arr, i):
 	returnArr = []
 	returnArr.append(arr[i])
@@ -73,6 +100,7 @@ def scc(graph, arr, i):
 
 #example.txt is smaller file
 #test.txt is bigger file
+#open the file to read the graph
 file = open("test.txt")
 for numberLine in file:
 	numberLine.strip()
@@ -85,6 +113,7 @@ for numberLine in file:
 	nodes[1] = temp
 	addEdge(graph, nodes[0], nodes[1])
 
+#create the graph
 arr = []
 DFS(arr, graph)
 #print(graph)
